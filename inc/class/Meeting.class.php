6310b012a92ad5453f7467aaeea4f470273fb363
LiteraTomáš <tomaslitera@hotmail.com>
2012-12-16 18:41:17 +0100

    Meeting extends Component class
    Refactor to OOP

--- inc/class/Meeting.class.php
+++ inc/class/Meeting.class.php
@@ -7,7 +7,7 @@
  * @created 2012-11-09
  * @author Tomas Litera <tomaslitera@hotmail.com>
  */
-class Meeting
+class Meeting extends Component
 {
 	/** @var meeting ID */
 	private $meetingId;
@@ -18,14 +18,14 @@ class Meeting
 	/**
 	 * Array of form names
 	 *
-	 * @var array	form_names[]
+	 * @var array	$form_names[]
 	 */
 	public $form_names = array();
 	
 	/**
 	 * Array of database programs table columns
 	 *
-	 * @var array	dbColumns[]
+	 * @var array	$dbColumns[]
 	 */
 	public $dbColumns = array();
 	
@@ -60,68 +60,7 @@ class Meeting
 			"advance",
 			"numbering"
 		);
-	}
-	
-	/**
-	 * Create a new meeting
-	 *
-	 * @return	boolean
-	 */
-	public function create($db_data)
-	{
-		$query_key_set = "";
-		$query_value_set = "";
-
-		foreach($db_data as $key => $value) {
-			$query_key_set .= "`".$key."`,";
-			$query_value_set .= "'".$value."',";
-		}
-		$query_key_set = substr($query_key_set, 0, -1);
-		$query_value_set = substr($query_value_set, 0, -1);	
-
-    	$query = "INSERT INTO `kk_meetings` 
-     				 (".$query_key_set.") 
-     				 VALUES (".$query_value_set.");";
-    	$result = mysql_query($query);
-
-		return $result;
-	}
-	
-	/**
-	 * Modify a meeting
-	 *
-	 * @param	int	ID of a meeting
-	 *
-	 * @return	bool
-	 */
-	public function modify($id, $db_data)
-	{
-	 	$query_set = "";
-	 	foreach($db_data as $key => $value) {
-			$query_set .= "`".$key."` = '".$value."',";	
-		}
-	 	$query_set = substr($query_set, 0, -1);	
-		
-    	echo $query = "UPDATE `kk_meetings` 
-					SET ".$query_set."
-					WHERE `id`='".$id."' LIMIT 1";
-    	$result = mysql_query($query);
-		
-		//return $result;
-	}
-	
-	/**
-	 * Delete meeting
-	 *
-	 * @param	int	ID of meeting
-	 * @return	boolean 
-	 */
-	public function delete($id)
-	{
-    	$query = "UPDATE kk_meetings SET deleted = '1' WHERE id = ".$id."	LIMIT 1";
-	    $result = mysql_query($query);
-		
-		return $result;
+		$this->dbTable = "kk_meetings";
 	}
 	
 	/**
