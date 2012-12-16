84f6e026292aaef2f2773c74f4877438e69fca91
LiteraTomáš <tomaslitera@hotmail.com>
2012-12-16 16:22:09 +0100

    Refactor of Meeting handler.
    Refactoring to OOP

---
+++ inc/class/Meeting.class.php
@@ -0,0 +1,343 @@
+<?php
+/**
+ * Meeting
+ *
+ * class for handling meeting
+ *
+ * @created 2012-11-09
+ * @author Tomas Litera <tomaslitera@hotmail.com>
+ */
+class Meeting
+{
+	/** @var meeting ID */
+	private $meetingId;
+	
+	/** @var days of weekend */
+	private $weekendDays = array();
+	
+	/**
+	 * Array of form names
+	 *
+	 * @var array	form_names[]
+	 */
+	public $form_names = array();
+	
+	/**
+	 * Array of database programs table columns
+	 *
+	 * @var array	dbColumns[]
+	 */
+	public $dbColumns = array();
+	
+	/** Constructor */
+	public function __construct($meetingId = NULL)
+	{
+		$this->meetingId = $meetingId;
+		$this->weekendDays = array("pátek", "sobota", "neděle");
+		$this->form_names = array(
+			"place",
+			"start_date",
+			"end_date",
+			"open_reg",
+			"close_reg",
+			"contact",
+			"email",
+			"gsm",
+			"cost",
+			"advance",
+			"numbering"
+		);
+		$this->dbColumns = array(
+			"place",
+			"start_date",
+			"end_date",
+			"open_reg",
+			"close_reg",
+			"contact",
+			"email",
+			"gsm",
+			"cost",
+			"advance",
+			"numbering"
+		);
+	}
+	
+	/**
+	 * Create a new meeting
+	 *
+	 * @return	boolean
+	 */
+	public function create($db_data)
+	{
+		$query_key_set = "";
+		$query_value_set = "";
+
+		foreach($db_data as $key => $value) {
+			$query_key_set .= "`".$key."`,";
+			$query_value_set .= "'".$value."',";
+		}
+		$query_key_set = substr($query_key_set, 0, -1);
+		$query_value_set = substr($query_value_set, 0, -1);	
+
+    	$query = "INSERT INTO `kk_meetings` 
+     				 (".$query_key_set.") 
+     				 VALUES (".$query_value_set.");";
+    	$result = mysql_query($query);
+
+		return $result;
+	}
+	
+	/**
+	 * Modify a meeting
+	 *
+	 * @param	int	ID of a meeting
+	 *
+	 * @return	bool
+	 */
+	public function modify($id, $db_data)
+	{
+	 	$query_set = "";
+	 	foreach($db_data as $key => $value) {
+			$query_set .= "`".$key."` = '".$value."',";	
+		}
+	 	$query_set = substr($query_set, 0, -1);	
+		
+    	echo $query = "UPDATE `kk_meetings` 
+					SET ".$query_set."
+					WHERE `id`='".$id."' LIMIT 1";
+    	$result = mysql_query($query);
+		
+		//return $result;
+	}
+	
+	/**
+	 * Delete meeting
+	 *
+	 * @param	int	ID of meeting
+	 * @return	boolean 
+	 */
+	public function delete($id)
+	{
+    	$query = "UPDATE kk_meetings SET deleted = '1' WHERE id = ".$id."	LIMIT 1";
+	    $result = mysql_query($query);
+		
+		return $result;
+	}
+	
+	/**
+	 * Get meeting price
+	 *
+	 * @var		string	type of charge
+	 * @return	int		return cost or false
+	 */
+	public function getPrice($type)
+	{
+		$query = "SELECT cost, advance FROM kk_meetings WHERE id='".$this->meetingId."' LIMIT 1";
+		$result = mysql_query($query);
+		$data = mysql_fetch_assoc($result);
+		
+		if($type == 'cost') return $data['cost'];
+		elseif($type == 'advance') return $data['advance'];
+		else return -1;
+	}
+	
+	/**
+	 * Render HTML Provinces <select>
+	 *
+	 * @param	int	ID of selected province
+	 * @return	string	html <select>
+	 */
+	public function renderHtmlProvinceSelect($selected_province)
+	{
+		$html_select = "<select style='width: 195px; font-size: 10px' name='province'>\n";
+
+		$query = "SELECT * FROM kk_provinces";
+		$result = mysql_query($query);
+		
+		while($data = mysql_fetch_assoc($result)){
+			if($data['id'] == $selected_province){
+				$sel = "selected";
+			}
+			else $sel = "";
+			$html_select .= "<option value='".$data['id']."' ".$sel.">".$data['province_name']."</option>";
+		}
+		
+		$html_select .= "</select>\n";
+				
+		return $html_select;
+	}
+
+	/**
+	 * Get Programs for Overview
+	 *
+	 * @return	string	html
+	 */
+	public function getPrograms($blockId)
+	{
+		$sql = "SELECT 	progs.id AS id,
+						progs.name AS name,
+						style
+				FROM kk_programs AS progs
+				LEFT JOIN kk_categories AS cat ON cat.id = progs.category
+				WHERE block='".$blockId."' AND progs.deleted='0'
+				LIMIT 10";
+		$result = mysql_query($sql);
+		$rows = mysql_affected_rows();
+	
+		if($rows == 0) $html = "";
+		else{
+			$html = "<table class='programs'>\n";
+			$html .= " <tr>\n";
+			while($data = mysql_fetch_assoc($result)){			
+				$html .= "<td class='cat-".$data['style']."' style='text-align:center;'>\n";
+				$html .= "<a class='program' href='../programs/process.php?id=".$data['id']."&cms=edit&page=meetings' title='".$data['name']."'>".$data['name']."</a>\n";
+				$html .= "</td>\n";
+			}
+			$html .= " </tr>\n";
+			$html .= "</table>\n";
+		}
+		return $html;
+	}
+	
+	/**
+	 * Render Program Overview
+	 *
+	 * @return	string	html
+	 */
+	public function renderProgramOverview()
+	{
+		$html = "";
+
+		foreach($this->weekendDays as $key => $value){
+			$html .= "<table class='blocks'>\n";
+			$html .= " <tr>\n";
+			$html .= "  <td class='day' colspan='2' >".$value."</td>\n";
+			$html .= " </tr>\n";
+		
+			$sql = "SELECT 	blocks.id AS id,
+							day,
+							DATE_FORMAT(`from`, '%H:%i') AS `from`,
+							DATE_FORMAT(`to`, '%H:%i') AS `to`,
+							blocks.name AS name,
+							program,
+							style
+					FROM kk_blocks AS blocks
+					LEFT JOIN kk_categories AS cat ON cat.id = blocks.category
+					WHERE blocks.deleted = '0' AND day='".$value."' AND meeting='".$this->meetingId."'
+					ORDER BY `from` ASC";
+		
+			$result = mysql_query($sql);
+			$rows = mysql_affected_rows();
+		
+			if($rows == 0){
+				$html .= "<td class='emptyTable' style='width:400px;'>Nejsou žádná aktuální data.</td>\n";
+			}
+			else{
+				while($data = mysql_fetch_assoc($result)){
+					$html .= "<tr>\n";
+					$html .= "<td class='time'>".$data['from']." - ".$data['to']."</td>\n";
+					if($data['program'] == 1){ 
+						$html .= "<td class='cat-".$data['style']."'>\n";
+						$html .= "<div>\n";
+						$html .= "<a class='block' href='".$GLOBALS['BLOCKDIR']."process.php?id=".$data['id']."&cms=edit&page=meetings' title='".$data['name']."'>".$data['name']."</a>\n";
+						$html .= "</div>\n";
+						$html .= $this->getPrograms($data['id']);
+						$html .= "</td>\n";
+					}
+					else {
+						$html .= "<td class='cat-".$data['style']."'>";
+						$html .= "<a class='block' href='".$GLOBALS['BLOCKDIR']."process.php?id=".$data['id']."&cms=edit&page=meetings' title='".$data['name']."'>".$data['name']."</a>\n";
+						$html .= "</td>\n";
+					}
+					$html .= "</tr>\n";
+				}
+			}
+			$html .= "</table>\n";
+		}
+		
+		return $html;
+	}
+	
+	/**
+	 * Render data in a table
+	 *
+	 * @return	string	html of a table
+	 */
+	public function renderData()
+	{
+		$sql = "SELECT 	id,
+						place,
+						DATE_FORMAT(start_date, '%d. %m. %Y') AS start_date,
+						DATE_FORMAT(end_date, '%d. %m. %Y') AS end_date,
+						DATE_FORMAT(open_reg, '%d. %m. %Y %H:%i:%s') AS open_reg,
+						DATE_FORMAT(close_reg, '%d. %m. %Y %H:%i:%s') AS close_reg,
+						contact,
+						email,
+						gsm
+				FROM kk_meetings
+				WHERE deleted = '0'
+				LIMIT 30";
+		$result = mysql_query($sql);
+		$rows = mysql_affected_rows();
+		
+		$html_row = "";
+		
+		if($rows == 0){
+			$html_row .= "<tr class='radek1'>";
+			$html_row .= "<td><img class='edit' src='".$GLOBALS['ICODIR']."small/edit2.gif' /></td>\n";
+			$html_row .= "<td><img class='edit' src='".$GLOBALS['ICODIR']."small/delete2.gif' /></td>\n";
+			$html_row .= "<td colspan='11' class='emptyTable'>Nejsou k dispozici žádné položky.</td>";
+			$html_row .= "</tr>";
+		}
+		else{
+			while($data = mysql_fetch_assoc($result)){			
+				$html_row .= "<tr class='radek1'>";
+				$html_row .= "<td><a href='process.php?id=".$data['id']."&cms=edit&page=meetings' title='Upravit'><img class='edit' src='".$GLOBALS['ICODIR']."small/edit.gif' /></a></td>\n";
+				$html_row .= "<td><a href=\"javascript:confirmation('?id=".$data['id']."&amp;cms=del', 'sraz: ".$data['place']." ".$data['start_date']." -> Opravdu SMAZAT tento sraz? Jste si jisti?')\" title='Odstranit'><img class='edit' src='".$GLOBALS['ICODIR']."small/delete.gif' /></a></td>\n";
+				$html_row .= "<td class='text'>".$data['id']."</td>\n";
+				$html_row .= "<td class='text'>".$data['place']."</td>\n";
+				$html_row .= "<td class='text'>".$data['start_date']."</td>\n";
+				$html_row .= "<td class='text'>".$data['end_date']."</td>\n";
+				$html_row .= "<td class='text'>".$data['open_reg']."</td>\n";
+				$html_row .= "<td class='text'>".$data['close_reg']."</td>\n";
+				$html_row .= "<td class='text'>".$data['contact']."</td>\n";
+				$html_row .= "<td class='text'>".$data['email']."</td>\n";
+				$html_row .= "<td class='text'>".$data['gsm']."</td>\n";
+				$html_row .= "</tr>";
+			}
+		}
+		
+		// table head
+		$html_thead = "<tr>\n";
+		$html_thead .= "<th></th>\n";
+		$html_thead .= "<th></th>\n";
+		$html_thead .= "<th class='tab1'>id</th>\n";
+		$html_thead .= "<th class='tab1'>místo</th>\n";
+		$html_thead .= "<th class='tab1'>začátek</th>\n";
+		$html_thead .= "<th class='tab1'>konec</th>\n";
+		$html_thead .= "<th class='tab1'>otevření registrace</th>\n";
+		$html_thead .= "<th class='tab1'>uzavření registrace</th>\n";
+		$html_thead .= "<th class='tab1'>kontakt</th>\n";
+		$html_thead .= "<th class='tab1'>e-mail</th>\n";
+		$html_thead .= "<th class='tab1'>telefon</th>\n";
+		$html_thead .= "</tr>\n";
+		
+		// table foot
+		$html_tfoot = $html_thead;
+
+		// table
+		$html_table = "<table id='MeetingsTable' class='list tablesorter'>\n";
+		$html_table .= "<thead>\n";
+		$html_table .= $html_thead;
+		$html_table .= "</thead>\n";
+		$html_table .= "<tfoot>\n";
+		$html_table .= $html_tfoot;
+		$html_table .= "</tfoot>\n";
+		$html_table .= "<tbody>\n";
+		$html_table .= $html_row;
+		$html_table .= "</tbody>\n";
+		$html_table .= "</table>\n";
+		
+		return $html_table;
+	}
+}
--- meetings/process.php
+++ meetings/process.php
@@ -1,107 +1,104 @@
 <?php
-
+//header
 require_once('../inc/define.inc.php');
 
 ######################### PRISTUPOVA PRAVA ################################
 
 include_once($INCDIR.'access.inc.php');
 
-###########################################################################
-
-if($mid = requested("mid","")){
-	$_SESSION['meetingID'] = $mid;
-} else {
-	$mid = $_SESSION['meetingID'];
-}
+######################### KONTROLA ########################################
 
 $id = requested("id","");
+$meetingId = $id;
+$page = requested("page","");
 $cms = requested("cms","");
-$error = requested("error","");
 
-$Container = new Container($GLOBALS['cfg'], $mid);
+$Container = new Container($GLOBALS['cfg'], $meetingId);
 $MeetingsHandler = $Container->createMeeting();
 
-// delete program
-if($cms == "del"){
-	if($MeetingsHandler->delete($id)){	
-	  	redirect("index.php?error=del");
-	}
-}
-
-if($cms == 'list-view'){
-	$heading1 = 'Správa srazů';
-	$heading2 = 'seznam srazů';	
-} else {
-	$heading1 = 'Aktuální sraz';
-	$heading2 = 'program';
-}
-
-$sql = "SELECT	*
-		FROM kk_meetings
-		WHERE id='".$mid."' AND deleted='0'
-		LIMIT 1";
-$result = mysql_query($sql);
-$dbData = mysql_fetch_assoc($result);
-
-foreach($MeetingsHandler->form_names as $key) {
-	$$key = requested($key, $dbData[$key]);
-}
-
 ////inicializace promenych
+$error = "";
 $error_start = "";
 $error_end = "";
 $error_open_reg = "";
 $error_close_reg = "";
 $error_login = "";
 
-// styles in header
-$style = Category::getStyles();
+######################## ZPRACOVANI ####################################
 
-############################## GENEROVANI STRANKY ##########################
+switch($cms) {
+	/* new meeting */
+	case "new":
+		$heading = "nový sraz";
+		$todo = "create";
+		
+		// requested for visitors fields
+		foreach($MeetingsHandler->dbColumns as $key) {
+			$$key = requested($key, "");	
+		}
+		break;
+	/* process creation of new meeting */
+	case "create":
+		// requested for meeting
+		foreach($MeetingsHandler->form_names as $key) {
+			$$key = requested($key, null);	
+		}
+		
+		foreach($MeetingsHandler->dbColumns as $key) {
+				$dbData[$key] = $$key;	
+		}
+		// create
+		if($MeetingsHandler->create($dbData)){	
+			redirect("../".$page."?error=ok");
+		}
+		break;
+	/* edit meeting */
+	case "edit":
+		$heading = "úprava srazu";
+		$todo = "modify";
+		// get meeting's data
+		$query = "SELECT	*
+					FROM kk_meetings
+					WHERE id='".$id."' AND deleted='0'
+					LIMIT 1"; 
+		$dbData = mysql_fetch_assoc(mysql_query($query));
+		
+		foreach($MeetingsHandler->dbColumns as $key) {
+			$$key = requested($key, $dbData[$key]);
+		}
+		
+		break;
+	/* process updating information about meeting */
+	case "modify":
+		foreach($MeetingsHandler->dbColumns as $key) {
+			$$key = requested($key, $value);
+			$dbData[$key] = $$key;
+		}
+		
+		if($MeetingsHandler->modify($id, $dbData)){	
+			redirect("../".$page."?error=ok");
+		}	
+		break;
+}
+
+################## GENEROVANI STRANKY #############################
 
 include_once($INCDIR.'http_header.inc.php');
 include_once($INCDIR.'header.inc.php');
 
-?>
+##################################################################
 
-<div class='siteContentRibbon'><?php echo $heading1; ?></div>
+?>
+<link href="../styles/css/default.css" rel="stylesheet" type="text/css" />
+
+<div class='siteContentRibbon'>Správa srazů</div>
 <?php printError($error); ?>
-<div class='pageRibbon'><?php echo $heading2; ?></div>
+<div class='pageRibbon'>úprava srazu</div>
 
-<script src='<?php echo $JSDIR ?>jquery/jquery.tablesorter.min.js' type='text/javascript'></script>
-<script>
-$(document).ready(function() {
-	$("#MeetingsTable").tablesorter( {
-		headers: {
-			0: { sorter: false},
-			1: { sorter: false},
-			5: { sorter: false},
-			6: { sorter: false},
-			7: { sorter: false},
-			9: { sorter: false},
-			10: { sorter: false},
-		}
-	} );
-} );
-</script>
-
-<?php 
-
-if($cms == 'list-view') {
-	echo "<div class='link'><a class='link' href='process.php?cms=new&page=meetings'><img src='".$ICODIR."small/new.png' />NOVÝ SRAZ</a></div>\n";
-	echo $MeetingsHandler->renderData();
-} else {
-	echo $MeetingsHandler->renderProgramOverview();
-}
-
-?>
-
-<div class='pageRibbon'>nastavení</div>
-
-<form action='process.php?redir=index' method='post'>
+<form action='process.php' method='post'>
 
 <div class='button-line'>
- <button type='submit' onclick=\"this.form.submit()\">
+ <button type='submit' onClick=\"this.form.submit()\">
   <img src='<?php echo $ICODIR; ?>small/save.png' /> Uložit</button>
  <button type='button' onclick="window.location.replace('index.php?cms=list-view')">
   <img src='<?php echo $ICODIR; ?>small/storno.png'  /> Storno</button>
@@ -159,20 +156,19 @@ $(function() {
  </tr>
  <tr>
   <td class='label'><label class="required">Začátek</label></td>
-  <td><div class="picker"><input id="start_date" class='datePicker' type='text' size='20' name='start_date' value="<?php echo $start_date; ?>" /></div><?php printError($error_start); ?>(rrrr-mm-dd)</td>
+  <td><div class="picker"><input id='start_date' class='datePicker' type='text' size='20' name='start_date' value="<?php echo $start_date; ?>" /></div><?php printError($error_start); ?>(rrrr-mm-dd)</td>
  </tr>
  <tr>
   <td class='label'><label class="required">Konec:</label></td>
-  <td><div class="picker"><input id='end_date' class="datePicker" type='text' size='20' name='end_date' value="<?php echo $end_date; ?>" /></div><?php printError($error_end); ?>(rrrr-mm-dd)</td>
+  <td><div class="picker"><input id='end_date' class='datePicker' type='text' size='20' name='end_date' value="<?php echo $end_date; ?>" /></div><?php printError($error_end); ?>(rrrr-mm-dd)</td>
  </tr>
  <tr>
   <td class='label'><label class="required">Otevřít přihlašování:</label></td>
-  <td><div class="picker"><input id="open_reg" class='dateTimePicker' type='text' size='30' name='open_reg' value="<?php echo $open_reg; ?>" /></div>
-  <?php printError($error_open_reg); ?>(rrrr-mm-dd hh:mm:ss)</td>
+  <td><div class="picker"><input id='open_reg' class='dateTimePicker' type='text' size='30' name='open_reg' value="<?php echo $open_reg; ?>" /></div><?php printError($error_open_reg); ?>(rrrr-mm-dd hh:mm:ss)</td>
  </tr>
  <tr>
   <td class='label'><label class="required">Zavřít přihlašování:</label></td>
-  <td><div class="picker"><input id="close_reg" class='dateTimePicker' type='text' size='30' name='close_reg' value="<?php echo $close_reg; ?>" /></div><?php printError($error_close_reg); ?>(rrrr-mm-dd hh:mm:ss)</td>
+  <td><div class="picker"><input id='close_reg' class='dateTimePicker' type='text' size='30' name='close_reg' value="<?php echo $close_reg; ?>" /></div><?php printError($error_close_reg); ?>(rrrr-mm-dd hh:mm:ss)</td>
  </tr>
  <tr>
   <td class='label'><label>Cena srazu:</label></td>
@@ -181,12 +177,11 @@ $(function() {
  <tr>
   <td class='label'><label>Záloha:</label></td>
   <td><input type='text' size='30' name='advance' value="<?php echo $advance; ?>" /> ,- Kč</td>
- </tr> 
- <tr>
+ </tr>
  <tr>
   <td class='label'><label>Číslování dokladů:</label></td>
   <td><input type='text' size='30' name='numbering' value="<?php echo $numbering; ?>" /></td>
- </tr> 
+ </tr>
  <tr>
   <td class='label'><label>Kontaktní osoba:</label></td>
   <td><input type='text' size='30' name='contact' value="<?php echo $contact; ?>" /><?php printError($error_close_reg); ?></td>
@@ -201,12 +196,12 @@ $(function() {
  </tr> 
 </table>
 
- <input type='hidden' name='cms' value='update'>
- <input type='hidden' name='mid' value='<?php echo $mid; ?>'>
+ <input type='hidden' name='cms' value='<?php echo $todo; ?>'>
+ <input type='hidden' name='page' value='<?php echo $page; ?>'>
+ <input type='hidden' name='id' value='<?php echo $id; ?>'>	
 </form>
 
 <?php
-
 ###################################################################
 
 include_once($INCDIR.'footer.inc.php');
