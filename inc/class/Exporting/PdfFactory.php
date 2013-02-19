efa77f328c5c47590e7ad9647ea57c5746a41f44
LiteraTomáš <tomaslitera@hotmail.com>
2013-02-19 18:00:28 +0100

    Factory class for Pdf
    Creates instance of mPDF with few settings.

---
+++ inc/class/Exporting/PdfFactory.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * PdfFactory
+ *
+ * factory to create instance of mPDF
+ *
+ * @created 2013-02-18
+ * @author Tomas Litera <tomaslitera@hotmail.com>
+ */
+class PdfFactory
+{
+	/** @var Pdf */
+	public $Pdf;
+	
+	/** @var configuration */
+	private $configuration;
+	
+	/** Constructor */
+	public function __construct(mPDF $Mpdf, $configuration)
+	{
+		$this->Pdf = $Mpdf;
+		$this->configuration = $configuration;
+	}
+	
+	/**
+	 * Return new Mpdf with few settings
+	 *
+	 * @return	Mpdf;
+	 */
+	public function create()
+	{
+		$this->Pdf->debug = true;
+		$this->Pdf->useOnlyCoreFonts = true;
+		$this->Pdf->SetDisplayMode('fullpage');
+		$this->Pdf->SetAutoFont(0);
+		$this->Pdf->defaultfooterfontsize = 16;
+		$this->Pdf->defaultfooterfontstyle = 'B';
+		
+		return $this->Pdf;
+	}
+}
