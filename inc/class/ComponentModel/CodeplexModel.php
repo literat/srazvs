04768b788063f3ad8c2d8ec6559ed5f0c13a28b0
LiteraTomáš <tomaslitera@hotmail.com>
2013-03-11 22:42:09 +0100

    Adding base CodeplexModel

---
+++ inc/class/ComponentModel/CodeplexModel.php
@@ -0,0 +1,30 @@
+﻿<?php
+
+/**
+ * CodeplexModel
+ *
+ * Default Model for base/default application Model (MVC)
+ *
+ * @created 2013-03-11
+ * @author Tomas Litera <tomaslitera@hotmail.com>
+ */ 
+abstract class CodeplexModel implements IModel
+{
+	/** Constructor */
+	public function __construct()
+	{
+	}
+	
+	/**
+	 * Create new or return existing instance of class
+	 *
+	 * @return	mixed	instance of class
+	 */
+	public static function getInstance()
+	{
+		if(self::$instance === false) {
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}	
+}
