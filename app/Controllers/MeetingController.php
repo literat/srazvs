7e48004c9c0873b2fca0c3f8b7395d0e6b43364f
LiteraTomáš <tomaslitera@hotmail.com>
2013-05-07 22:52:44 +0200

    Moving and renaming files [refs #3]
    Preparing MVC structure base on Nette framework and renaming classes to that they actualy represent.

---
+++ app/Controllers/MeetingController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file handles the retrieval and serving of news articles
+ */
+class MeetingController
+{
+	/**
+	 * This template variable will hold the 'view' portion of our MVC for this 
+	 * controller
+	 */
+	public $template = 'nmeeting';
+
+	/**
+	 * This is the default function that will be called by router.php
+	 * 
+	 * @param array $getVars the GET variables posted to index.php
+	 */
+	public function main(array $getVars)
+	{
+		//this is a test , and we will be removing it later
+		print "We are in news!";
+		print '<br/>';
+		$vars = print_r($getVars, TRUE);
+		print 
+		(
+			"The following GET vars were passed to this controller:" .
+			"<pre>".$vars."</pre>"
+		);
+	}
+}
