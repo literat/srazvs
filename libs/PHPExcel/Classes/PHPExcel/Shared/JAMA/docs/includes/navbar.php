187ec4743eebacb980c96993729ddf4d2dce9e2d
LiteraTomáš <tomaslitera@hotmail.com>
2013-10-17 15:51:58 +0200

    Updating PHPExcel library (refs #3)

--- libs/PHPExcel/Classes/PHPExcel/Shared/JAMA/docs/includes/navbar.php
+++
@@ -1,5 +0,0 @@
-<center>
-  <hr />
-    [ <a href="../../../../../../../../../srazvs/inc/phpexcel/Classes/PHPExcel/Shared/JAMA/docs/includes/index.php">index.php</a> ] [ <a href="../../../../../../../../../srazvs/inc/phpexcel/Classes/PHPExcel/Shared/JAMA/docs/includes/docs.php">docs.php</a> ] [ <a href="../../../../../../../../../srazvs/inc/phpexcel/Classes/PHPExcel/Shared/JAMA/docs/includes/package.php">package.php</a> ] [ <a href="../../../../../../../../../srazvs/inc/phpexcel/Classes/PHPExcel/Shared/JAMA/docs/includes/test.php">test.php</a> ] [ <a href="../../../../../../../../../srazvs/inc/phpexcel/Classes/PHPExcel/Shared/JAMA/docs/includes/example.php">example.php</a> ] [ <a href="../../../../../../../../../srazvs/inc/phpexcel/Classes/PHPExcel/Shared/JAMA/docs/includes/download.php">download.php</a> ]
-  <hr />
-</center>
