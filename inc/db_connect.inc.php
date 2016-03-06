37dd696b378a429cad0efcfa62fefb196be0b386
literat <tomas@litera.me>
2016-03-06 09:56:46 +0100

    fix(indentation): Convert spaces to tabs (refs #33)

--- inc/db_connect.inc.php
+++ inc/db_connect.inc.php
@@ -11,27 +11,27 @@
  * @param string $encoding - kodovani pouzite pro spojeni
  * @return int $connected - vraci 0 (false) nebo 1 (true)
  */
- 
-function connectMySQL($host, $user, $password, $db, $encoding){
-           //pripojeni
-           $result = mysql_connect ($host, $user, $password);
-		   $result = mysql_select_db ($db);
-		   //chybovy stav
-		   //pripojeno
-           if($result == true) $connected = true;
-           //nepripojeno - chyba
-		   else {
-			   $connected = false;
-               die;
-		   }	 
-           //nastaveni kodovani
-           mysql_query("SET NAMES '".$encoding."'");
-		   mysql_query("SET CHARACTER SET '".$encoding."'");
-           mysql_query("set session character_set_result = '$encoding'");
-           mysql_query("set session character_set_client = '$encoding'");
-           mysql_query("set session character_set_connection = '$encoding'");
-		   //navratova hodnota
-		   return $connected;
+
+function connectMySQL($host, $user, $password, $db, $encoding) {
+	//pripojeni
+	$result = mysql_connect ($host, $user, $password);
+	$result = mysql_select_db ($db);
+	//chybovy stav
+	//pripojeno
+	if($result == true) $connected = true;
+		//nepripojeno - chyba
+	else {
+		$connected = false;
+		die;
+	}
+	//nastaveni kodovani
+	mysql_query("SET NAMES '".$encoding."'");
+	mysql_query("SET CHARACTER SET '".$encoding."'");
+	mysql_query("set session character_set_result = '$encoding'");
+	mysql_query("set session character_set_client = '$encoding'");
+	mysql_query("set session character_set_connection = '$encoding'");
+	//navratova hodnota
+	return $connected;
 };
 
 $error = connectMySQL($cfg['db_host'], $cfg['db_user'], $cfg['db_passwd'], $cfg['db_database'], $cfg['encoding']);
