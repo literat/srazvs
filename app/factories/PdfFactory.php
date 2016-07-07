fb7b22569dc0e445b2eef0c101dbbb52abab3688
literat <tomas@litera.me>
2016-07-07 11:33:22 +0200

    feat(cleaning libs): Added pdf factory for using Mpdf (refs #47)

---
+++ app/factories/PdfFactory.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App;
+
+use Mpdf\Mpdf;
+
+/**
+ * PdfFactory
+ *
+ * factory to create instance of Mpdf
+ *
+ * @created 2016-07-07
+ * @author Tomas Litera <tomas@litera.me>
+ */
+class PdfFactory
+{
+	/** @var Pdf */
+	public $pdf;
+
+	/** @var defaults */
+	private $encoding = 'utf-8';
+	private $paperFormat = 'A4';
+	private $fontSize = 0;
+	private $font = '';
+	private $marginLeft = 15;
+	private $marginRight = 15;
+	private $marginTop = 16;
+	private $marginBottom = 16;
+
+	/** Constructor */
+	public function __construct(array $configuration)
+	{
+		$this->encoding 	= $configuration['encoding'];
+		$this->paperFormat 	= $configuration['paperFormat'];
+		$this->fontSize 	= $configuration['fontSize'];
+		$this->font 		= $configuration['font'];
+		$this->marginLeft 	= $configuration['marginLeft'];
+		$this->marginRight 	= $configuration['marginRight'];
+		$this->marginTop 	= $configuration['marginTop'];
+		$this->marginBottom = $configuration['marginBottom'];
+	}
+
+	/**
+	 * Return new Mpdf with few settings
+	 *
+	 * @return	Mpdf;
+	 */
+	public function create()
+	{
+		$this->pdf = new \Mpdf(
+			$this->encoding,
+			$this->paperFormat,
+			$this->fontSize,
+			$this->font,
+			$this->marginLeft,
+			$this->marginRight,
+			$this->marginTop,
+			$this->marginBottom
+		);
+
+		// debugging on demand
+		if(defined('DEBUG') && DEBUG === TRUE){
+			$this->pdf->debug = true;
+		}
+		$this->pdf->useOnlyCoreFonts = true;
+		$this->pdf->SetDisplayMode('fullpage');
+		$this->pdf->SetAutoFont(0);
+		$this->pdf->defaultfooterfontsize = 16;
+		$this->pdf->defaultfooterfontstyle = 'B';
+
+		return $this->pdf;
+	}
+
+	/**
+	 * Set margins of PDF
+	 */
+	public function setMargins($left, $right, $top, $bottom)
+	{
+		$this->marginLeft 	= $left;
+		$this->marginRight 	= $right;
+		$this->marginTop 	= $top;
+		$this->marginBottom = $bottom;
+	}
+
+	/**
+	 * Set paper format of PDF
+	 */
+	public function setPaperFormat($paperFormat)
+	{
+		$this->paperFormat = $paperFormat;
+	}
+}
