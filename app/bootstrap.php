68088224c84151a895e4f54cbfef904950edf11b
literat <tomas@litera.me>
2016-07-22 00:20:01 +0200

    feat(nette bootstrap): Using configurator and moved start sequence to bootstrap file (refs #50)

---
+++ app/bootstrap.php
@@ -0,0 +1,126 @@
+<?php
+
+use Tracy\Debugger;
+use Nette\Configurator;
+use Nette\DI\ContainerLoader;
+use Nette\Database\Context;
+use Nette\Caching\Storages\FileStorage;
+use Nette\Database\Structure;
+use Nette\Loaders\RobotLoader;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../inc/define.inc.php';
+
+$configurator = new Configurator;
+
+/**
+ * Enabling Debugger
+ */
+//$configurator->setDebugMode(TRUE);
+$configurator->enableDebugger(__DIR__ . '/../temp/log', $cfg['mail-admin']);
+$configurator->setTempDirectory(__DIR__ . '/../temp');
+
+/**
+ * Autoloading
+ */
+$configurator->createRobotLoader()
+	->addDirectory(__DIR__)
+	->addDirectory(__DIR__ . '/../inc')
+	->register();
+
+if ($configurator->isDebugMode()) {
+    $configurator->addConfig(__DIR__ . '/config/config.development.neon');
+} else {
+    $configurator->addConfig(__DIR__ . '/config/config.production.neon');
+}
+$configurator->addConfig(__DIR__ . '/config/config.local.neon');
+
+/**
+ * DI Container
+ */
+$container = $configurator->createContainer();
+
+/**
+ * Connecting to Database
+ */
+$connection = $container->createServiceConnection();
+$database = $container->createServiceDatabase();
+
+
+
+// Tracy database panel
+Nette\Database\Helpers::createDebugPanel($connection);
+
+if(!isset($_SESSION['meetingID'])) {
+	$_SESSION['meetingID'] = $database
+		->table('kk_meetings')
+		->select('id')
+		->order('id DESC')
+		->limit(1)
+		->fetchField();
+}
+
+$router = new Router();
+
+$router->setDefaults(array(
+    'controller' => 'meeting',
+    'id' => $_SESSION['meetingID'],
+));
+
+/**
+ first route that match is used
+*/
+$router->connect('/', array(), true, true);
+//$router->connect('/<:controller>/<:action>', array(), true, true);
+$router->connect('/<:controller>', array(), true, true);
+$router->connect('/<:controller>/<:action>', array(), true, true);
+$routing = $router->getRouting();
+
+$target = CONTROLLER_DIR.$routing['controller'].'Controller.php';
+
+//get target
+if(file_exists($target))
+{
+	//print_r(get_declared_classes());
+	if(
+		(
+			$routing['controller'] != 'Registration'
+			&& (!isset($_GET['cms']) || $_GET['cms'] != 'public')
+		)
+			&& (!isset($_GET['cms']) || $_GET['cms'] != 'annotation')
+			&& (!isset($_POST['page']) || $_POST['page'] != 'annotation')
+	) {
+		include_once(INC_DIR.'access.inc.php');
+	}
+	require_once($target);
+
+	//modify page to fit naming convention
+	$class = $routing['controller'].'Controller';
+
+	//instantiate the appropriate class
+	if(class_exists($class))
+	{
+		$controller = new $class($database, $container);
+		$controller->setRouting($routing);
+	}
+	else
+	{
+		//did we name our class correctly?
+		die('class does not exist!');
+	}
+}
+else
+{
+	//can't find the file in 'controllers'!
+	die('page does not exist!');
+}
+
+$getParams = $router->getParams();
+
+//if(empty($getParams)) {
+//	redirect('?mid='.$data['id']);
+//}
+
+//once we have the controller instantiated, execute the default function
+//pass any GET varaibles to the main method
+$controller->init($getParams);
