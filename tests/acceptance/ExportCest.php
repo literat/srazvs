8bf70eba5f4791bb84d8537b46020be73d770cc5
literat <tomas@litera.me>
2016-08-14 22:34:56 +0200

    feat(definitions refactor): Added acceptance tests for exports (refs #53)

--- tests/acceptance/ExportCest.php
+++ tests/acceptance/ExportCest.php
@@ -50,6 +50,22 @@ class ExportCest
 		$I->see('Převzal:');
 	}
 
+	public function it_should_export_evidence_summary(AcceptanceTester $I)
+	{
+		$I->wantTo('Export evidence summary');
+		$I->amOnPage('/srazvs/export/?mid=1');
+		$I->seeInCurrentUrl('/srazvs/export/');
+		$I->click('kompletní příjmový pokladní doklad');
+		$I->seeInCurrentUrl('/srazvs/export/?evidence=summary');
+		$I->see('Příjmení a Jméno');
+		$I->see('Narození');
+		$I->see('Adresa');
+		$I->see('Jednotka');
+		$I->see('Záloha');
+		$I->see('Doplatek');
+		$I->see('Podpis');
+	}
+
 	public function it_should_export_public_program(AcceptanceTester $I)
 	{
 		$I->wantTo('Export public program');
@@ -91,4 +107,87 @@ class ExportCest
 		$I->see('neděle');
 		$I->see('DEBUG_MODE');
 	}
+
+	public function it_should_export_meeting_shedule_into_badge(AcceptanceTester $I)
+	{
+		$I->wantTo('Export meeting schedule into visitor`s badge');
+		$I->amOnPage('/srazvs/export/?mid=1');
+		$I->seeInCurrentUrl('/srazvs/export/');
+		$I->click('program srazu - do visačky');
+		$I->seeInCurrentUrl('/srazvs/export/?program-badge');
+		$I->see('pátek');
+		$I->see('sobota');
+		$I->see('neděle');
+		$I->see('DEBUG_MODE');
+	}
+
+	public function it_should_export_name_badges(AcceptanceTester $I)
+	{
+		$I->wantTo('Export name badges');
+		$I->amOnPage('/srazvs/export/?mid=1');
+		$I->seeInCurrentUrl('/srazvs/export/');
+		$I->click('jmenovky');
+		$I->seeInCurrentUrl('/srazvs/export/?name-badges');
+		$I->see('roboTester');
+		$I->see('DEBUG_MODE');
+	}
+
+	public function it_should_export_attendance_list(AcceptanceTester $I)
+	{
+		$I->wantTo('Export attendance list');
+		$I->amOnPage('/srazvs/export/?mid=1');
+		$I->seeInCurrentUrl('/srazvs/export/');
+		$I->click('prezenční listina');
+		$I->seeInCurrentUrl('/srazvs/export/?attendance');
+		$I->see('Příjmení a Jméno');
+		$I->see('Narození');
+		$I->see('Adresa');
+		$I->see('Středisko/Přístav');
+		$I->see('Podpis');
+		$I->see('DEBUG_MODE');
+	}
+
+	public function it_should_export_name_list(AcceptanceTester $I)
+	{
+		$I->wantTo('Export name list');
+		$I->amOnPage('/srazvs/export/?mid=1');
+		$I->seeInCurrentUrl('/srazvs/export/');
+		$I->click('jmenný seznam');
+		$I->seeInCurrentUrl('/srazvs/export/?name-list');
+		$I->see('Příjmení, Jméno, Přezdívka');
+		$I->see('Adresa');
+		$I->see('Středisko/Přístav');
+		$I->see('DEBUG_MODE');
+	}
+
+	public function it_should_export_meal_tickets(AcceptanceTester $I)
+	{
+		$I->wantTo('Export meal tickets');
+		$I->amOnPage('/srazvs/export/?mid=1');
+		$I->seeInCurrentUrl('/srazvs/export/');
+		$I->click('stravenky');
+		$I->seeInCurrentUrl('/srazvs/export/?meal-ticket');
+		$I->see('Pátek');
+		$I->see('Sobota');
+		$I->see('Neděle');
+		$I->see('Snídaně');
+		$I->see('Oběd');
+		$I->see('Večeře');
+		$I->see('DEBUG_MODE');
+	}
+
+	public function it_should_export_program_details(AcceptanceTester $I)
+	{
+		$I->wantTo('Export program details');
+		$I->amOnPage('/srazvs/export/?mid=1');
+		$I->seeInCurrentUrl('/srazvs/export/');
+		$I->click('detaily programů');
+		$I->seeInCurrentUrl('/srazvs/export/?program-details');
+		$I->see('Program:');
+		$I->see('Popis:');
+		$I->see('Lektor:');
+		$I->see('E-mail:');
+		$I->see('DEBUG_MODE');
+	}
+
 }
