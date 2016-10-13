3861df1dbf6837185fb1319efeb748c102df3081
literat <tomas@litera.me>
2016-10-13 21:00:51 +0200

    fix(meeting creation): Fix nonexting key in array (refs #19)

--- tests/acceptance/_bootstrap.php
+++ tests/acceptance/_bootstrap.php
@@ -9,21 +9,31 @@ class CestCase
 
 	protected function fillForm(\AcceptanceTester $I, array $formData)
 	{
-		foreach($formData['fields'] as $field => $value) {
-			$I->fillField($field, $value);
+		if(array_key_exists('fields', $formData)) {
+			foreach($formData['fields'] as $field => $value) {
+				$I->fillField($field, $value);
+			}
 		}
-		foreach($formData['options'] as $option => $value) {
-			$I->selectOption($option, $value);
+
+		if(array_key_exists('options', $formData)) {
+			foreach($formData['options'] as $option => $value) {
+				$I->selectOption($option, $value);
+			}
 		}
 	}
 
 	protected function seeInForm(\AcceptanceTester $I, array $formData)
 	{
-		foreach($formData['fields'] as $field => $value) {
-			$I->seeInField($field, $value);
+		if(array_key_exists('fields', $formData)) {
+			foreach($formData['fields'] as $field => $value) {
+				$I->seeInField($field, $value);
+			}
 		}
-		foreach($formData['options'] as $option => $value) {
-			$I->seeOptionIsSelected($option, $value);
+
+		if(array_key_exists('options', $formData)) {
+			foreach($formData['options'] as $option => $value) {
+				$I->seeOptionIsSelected($option, $value);
+			}
 		}
 	}
 
