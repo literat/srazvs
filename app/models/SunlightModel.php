b96974ba7424c0075405f081fab0e9f344dc9bdb
literat <tomas@litera.me>
2016-12-12 20:55:33 +0100

    feat(Migration to presenters): Model for sunlight part of database (refs #79)

---
+++ app/models/SunlightModel.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App;
+
+use Tracy\Debugger;
+use Nette\Utils\Strings;
+use Nette\Database\Context;
+
+/**
+ * Sunlight
+ *
+ * class for handling sunlight CMS data
+ *
+ * @created 2016-12-06
+ * @author Tomas Litera <tomaslitera@hotmail.com>
+ */
+class SunlightModel extends BaseModel
+{
+
+	/** @var string */
+	protected $table = 'sunlight-users';
+
+	/**
+	 * @param Nette\Database\Context $database
+	 */
+	public function __construct(Context $database)
+	{
+		$this->setDatabase($database);
+	}
+
+	/**
+	 * @param  int $uid
+	 * @return ActiveRow
+	 */
+	public function findUser($uid)
+	{
+		return $this->getDatabase()
+			->table($this->getTable())
+			->where('id', $uid)
+			->fetch();
+	}
+
+}
