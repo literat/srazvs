8247a6bd16a28e044e76dc95942870f0a93fcf97
literat <tomas@litera.me>
2016-12-16 23:27:42 +0100

    refactor(Migration to presenters): Added router factory (refs #79)

---
+++ app/router/RouterFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Routers;
+
+use Nette,
+	Nette\Application\Routers\RouteList,
+	Nette\Application\Routers\Route,
+	Nette\Application\Routers\SimpleRouter;
+
+
+/**
+ * Router factory.
+ */
+class RouterFactory
+{
+
+	/**
+	 * @return \Nette\Application\IRouter
+	 */
+	public function createRouter()
+	{
+		$router = new RouteList();
+		$router[] = new Route('index.php', 'Dashboard:default', Route::ONE_WAY);
+		$router[] = new Route('<presenter>/<action>[/<id>]', 'Meeting:listing');
+
+		return $router;
+	}
+
+}
