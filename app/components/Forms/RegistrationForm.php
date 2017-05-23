ce6b3130d00ca8e025f4b4b20a1e01ec56577e19
literat <tomas@litera.me>
2017-05-23 16:23:17 +0200

    refactor(Registration): Setup form rendering (refs #95)

--- app/components/Forms/RegistrationForm.php
+++ app/components/Forms/RegistrationForm.php
@@ -79,9 +79,6 @@ class RegistrationForm extends BaseForm
 
 		$template = $this->getTemplate();
 		$template->setFile($this->buildTemplatePath());
-		$template->imgDir = IMG_DIR;
-		$template->wwwDir = HTTP_DIR;
-		$template->jsDir = JS_DIR;
 		$template->meals = $this->getMealFields();
 		$template->programs = $this->getProgramFields();
 		$template->render();
@@ -169,25 +166,7 @@ class RegistrationForm extends BaseForm
 		$form->addSubmit('reset', 'storno')
 			->setAttribute('class', 'btn-reset');
 
-		// setup form rendering
-		$renderer = $form->getRenderer();
-		$renderer->wrappers['controls']['container'] = NULL;
-		$renderer->wrappers['pair']['.error'] = 'has-error';
-		$renderer->wrappers['control']['description'] = 'span class=help-block';
-		$renderer->wrappers['control']['errorcontainer'] = 'span class=help-block';
-
-		// make form and controls compatible with Twitter Bootstrap
-		$form->getElementPrototype()->class('form-horizontal');
-		foreach ($form->getControls() as $control) {
-			if ($control instanceof Controls\Button) {
-				$control->getControlPrototype()->addClass(empty($usedPrimary) ? 'btn btn-default' : '');
-				$usedPrimary = TRUE;
-			} elseif ($control instanceof Controls\TextBase || $control instanceof Controls\SelectBox || $control instanceof Controls\MultiSelectBox) {
-				$control->getControlPrototype()->addClass('form-control');
-			} elseif ($control instanceof Controls\Checkbox || $control instanceof Controls\CheckboxList || $control instanceof Controls\RadioList) {
-				$control->getSeparatorPrototype()->setName('div')->addClass($control->getControlPrototype()->type);
-			}
-		}
+		$form = $this->setupRendering($form);
 
 		$form->onSuccess[] = [$this, 'processForm'];
 
@@ -204,6 +183,47 @@ class RegistrationForm extends BaseForm
 		$registration['meeting'] = $this->getMeetingId();
 
 		$this->onRegistrationSave($this, $registration);
+	}
+
+	/**
+	 * @param  Form   $form
+	 * @return Form
+	 */
+	protected function setupRendering(Form $form): Form
+	{
+		// setup form rendering
+		$renderer = $form->getRenderer();
+		$renderer->wrappers['controls']['container'] = NULL;
+		$renderer->wrappers['pair']['.error'] = 'has-error';
+		$renderer->wrappers['control']['description'] = 'span class=help-block';
+		$renderer->wrappers['control']['errorcontainer'] = 'span class=help-block';
+
+		// make form and controls compatible with Twitter Bootstrap
+		$form->getElementPrototype()->class('form-horizontal');
+		foreach ($form->getControls() as $control) {
+			if ($control instanceof Controls\Button) {
+				$control->getControlPrototype()
+					->addClass(empty($usedPrimary) ? 'btn btn-default' : '');
+				$usedPrimary = TRUE;
+			} elseif (
+				$control instanceof Controls\TextBase ||
+				$control instanceof Controls\SelectBox ||
+				$control instanceof Controls\MultiSelectBox
+			) {
+				$control->getControlPrototype()
+					->addClass('form-control');
+			} elseif (
+				$control instanceof Controls\Checkbox ||
+				$control instanceof Controls\CheckboxList ||
+				$control instanceof Controls\RadioList
+			) {
+				$control->getSeparatorPrototype()
+					->setName('div')
+					->addClass($control->getControlPrototype()->type);
+			}
+		}
+
+		return $form;
 	}
 
 	/**
