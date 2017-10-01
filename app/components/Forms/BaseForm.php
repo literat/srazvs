01afdc1d22a09e929182544112e5114b564cedb3
literat <tomas@litera.me>
2017-10-01 23:01:27 +0200

    feat(Annotation Form): Create new form and refactor setupRendering (refs #114)

--- app/components/Forms/BaseForm.php
+++ app/components/Forms/BaseForm.php
@@ -3,10 +3,53 @@
 namespace App\Components\Forms;
 
 use App\Components\BaseControl;
+use Nette\Application\UI\Form;
+use Nette\Forms\Controls;
 
 abstract class BaseForm extends BaseControl
 {
 
 	const TEMPLATE_DIR = __DIR__ . '/../../templates/components/Forms';
+
+	/**
+	 * @param  Form   $form
+	 * @return Form
+	 */
+	protected function setupRendering(Form $form): Form
+	{
+		// setup form rendering
+		$renderer = $form->getRenderer();
+		$renderer->wrappers['controls']['container'] = NULL;
+		$renderer->wrappers['pair']['.error'] = 'has-error';
+		$renderer->wrappers['control']['description'] = 'span class=help-block';
+		$renderer->wrappers['control']['errorcontainer'] = 'span class=help-block';
+
+		// make form and controls compatible with Twitter Bootstrap
+		$form->getElementPrototype()->class('form-horizontal');
+		foreach ($form->getControls() as $control) {
+			if ($control instanceof Controls\Button) {
+				$control->getControlPrototype()
+					->addClass(empty($usedPrimary) ? 'btn btn-default' : '');
+				$usedPrimary = TRUE;
+			} elseif (
+				$control instanceof Controls\TextBase ||
+				$control instanceof Controls\SelectBox ||
+				$control instanceof Controls\MultiSelectBox
+			) {
+				$control->getControlPrototype()
+					->addClass('form-control');
+			} elseif (
+				$control instanceof Controls\Checkbox ||
+				$control instanceof Controls\CheckboxList ||
+				$control instanceof Controls\RadioList
+			) {
+				$control->getSeparatorPrototype()
+					->setName('div')
+					->addClass($control->getControlPrototype()->type);
+			}
+		}
+
+		return $form;
+	}
 
 }
--- app/components/Forms/RegistrationForm.php
+++ app/components/Forms/RegistrationForm.php
@@ -10,7 +10,6 @@ use App\Models\BlockModel;
 use App\Models\MealModel;
 use App\Models\MeetingModel;
 use Nette\Application\UI\Form;
-use Nette\Forms\Controls;
 use App\Services\SkautIS\UserService;
 
 class RegistrationForm extends BaseForm
@@ -204,47 +203,6 @@ class RegistrationForm extends BaseForm
 		$registration['meeting'] = $this->getMeetingId();
 
 		$this->onRegistrationSave($this, $registration);
-	}
-
-	/**
-	 * @param  Form   $form
-	 * @return Form
-	 */
-	protected function setupRendering(Form $form): Form
-	{
-		// setup form rendering
-		$renderer = $form->getRenderer();
-		$renderer->wrappers['controls']['container'] = NULL;
-		$renderer->wrappers['pair']['.error'] = 'has-error';
-		$renderer->wrappers['control']['description'] = 'span class=help-block';
-		$renderer->wrappers['control']['errorcontainer'] = 'span class=help-block';
-
-		// make form and controls compatible with Twitter Bootstrap
-		$form->getElementPrototype()->class('form-horizontal');
-		foreach ($form->getControls() as $control) {
-			if ($control instanceof Controls\Button) {
-				$control->getControlPrototype()
-					->addClass(empty($usedPrimary) ? 'btn btn-default' : '');
-				$usedPrimary = TRUE;
-			} elseif (
-				$control instanceof Controls\TextBase ||
-				$control instanceof Controls\SelectBox ||
-				$control instanceof Controls\MultiSelectBox
-			) {
-				$control->getControlPrototype()
-					->addClass('form-control');
-			} elseif (
-				$control instanceof Controls\Checkbox ||
-				$control instanceof Controls\CheckboxList ||
-				$control instanceof Controls\RadioList
-			) {
-				$control->getSeparatorPrototype()
-					->setName('div')
-					->addClass($control->getControlPrototype()->type);
-			}
-		}
-
-		return $form;
 	}
 
 	/**
